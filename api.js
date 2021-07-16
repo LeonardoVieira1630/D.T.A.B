ba9a3be4985b14f22b99f519bc920bfdc746380c
Leonardo Vieira <69486932+LeonardoVieira1630@users.noreply.github.com>
2021-07-16 15:40:24 -0300

    Atualização
    Agora o DTAB pode fazer compra e venda propriamente dita.

--- api.js
+++ api.js
@@ -1,7 +1,45 @@
 //Contem as funções de comunicação com a api da binance
 
-const axios = require("axios")
-const querystring = require('querystring')
+
+const axios = require('axios');
+const crypto  = require('crypto');
+const querystring = require('querystring');
+
+const apiKey = process.env.API_KEY;
+const apiSecret = process.env.SECRET_KEY
+const apiUrl = process.env.API_URL
+
+//Func para requisições privadas (compra, venda e afins)
+async function privateCall(path, data = {}, method = 'GET'){
+
+    //Para todas as chamadas é necessário passar o instante que ela foi gerada, logo:
+    const timestamp = Date.now();
+    //Usando a biblioteca crypto e o "gerador" de requisição HMAC, cripitografamos a requisição que vamos mandar
+    const signature = crypto.createHmac('sha256', apiSecret)
+                    .update(`${querystring.stringify({...data, timestamp})}`)
+                    .digest('hex');
+
+    const newData = {...data, timestamp , signature};
+    const qs = `?${querystring.stringify(newData)}`
+
+    try{
+        const result = await axios({
+            method,
+            url: `${apiUrl}${path}${qs}`,
+            headers: { 'X-MBX-APIKEY': apiKey}
+        })
+        return result.data
+    }
+    catch(err){
+        console.log(err)
+    }
+}
+
+
+async function accountInfo(){
+    return privateCall('/v3/account');
+}
+
 
 
 //Func que faz requisição para a binance.
@@ -17,19 +55,33 @@ async function publicCall(path, data, method = 'GET'){
         return result.data;
     }
     catch(err){
-        console.log(err)
+        //console.log(err)
     }
 }
 
-/*
+
+async function newOrder(symbol, quantity, price, side = 'BUY', type= 'MARKET'){
+    const data = {symbol, side, type, quantity};
+
+    if (price) data.price = price;
+    if (type == 'LIMIT') data.timeInForce = 'GTC';
+
+    return privateCall ('/v3/order', data, 'POST')
+}
+
+
 async function time(){
     return publicCall('/v3/time');
 }
-*/
+
+async function exchangeInfo(symbol) {
+    const result = await publicCall('/v3/exchangeInfo');
+    return symbol ? result.symbols.find(s => s.symbol === symbol) : result.symbols;
+}
 
 //Func que pede os dados do BTC.
 async function depth(symbol = 'BTCBRL', limit =5){
     return publicCall('/v3/depth', {symbol, limit})
 }
 
-module.exports = {depth}
+module.exports = {time, depth, exchangeInfo, accountInfo, newOrder}
--- index.js
+++ index.js
@@ -1,23 +1,66 @@
 //require('dotenv').config()
 /*Executando código dentro do time out a cada instante determinado la no .env.
 Pegamos esse instante usando "process.env.CRAWLER_INTERVAL".*/
-const { parse } = require('dotenv');
+//const { parse } = require('dotenv');
 const api = require('./api')
+const symbol = process.env.SYMBOL
+const apiUrl = process.env.API_URL
+const profitability = process.env.PROFITABILITY
+const coin = process.env.COIN
+
+
+
+
+console.log('Iniciando monitoramento!');
+
 
 //Essa func deve ser assíncrona para esperar a resposta da func chamada antes do console.
 setInterval(async() => {
-    
-    const result = await api.depth()
-    console.log(`Maior ordem de compra atualmente: ${result.bids[0][0]}`)
-    console.log(`Menor ordem de venda atualmente: ${result.asks[0][0]}`)
 
-    const buy = parseInt(result.bids[0][0])
-    const sell = parseInt(result.asks[0][0])
+    console.log(`Mercado para ${symbol}`);
+    let sell=0, buy=0;
+    const result = await api.depth(symbol)
+    if(result.bids && result.bids.length){
+        console.log(`Maior ordem de compra atualmente: ${result.bids[0][0]}`)
+        buy = parseFloat(result.bids[0][0])
+    }
+    if(result.asks && result.asks.length){
+        console.log(`Menor ordem de venda atualmente: ${result.asks[0][0]}`)
+        sell = parseFloat(result.asks[0][0])
+    }
 
-    if (sell<20000) console.log('Devemos comprar!')
-    else if(buy>22000) console.log('Hora de vender!')
+    // alterar esse valor para um valor condizente 
+    if (sell<700) {
+        console.log('Devemos comprar!')
+        const account = await api.accountInfo();
+        const coins = account.balances.filter(b => symbol.indexOf(b.asset) !== -1)
+        console.log('Posição da carteira: ')
+        console.log(coins);
+
+        //Para outras moedas (btc), revisar essa parte no 2 - 8
+        console.log('Verificando se tenho dinheiro...')
+        const carteiraUSD = parseFloat(coins.find(c => c.asset === coin).free);
+        if(sell <= carteiraUSD){
+
+            console.log('Tenho! Comprando!');
+            const buyOrder = await api.newOrder(symbol, 1);
+            console.log(`orderId: ${buyOrder.orderId}`);
+            console.log(`status: ${buyOrder.status}`);
+
+            const price = parseFloat(sell * profitability).toFixed(8);
+            console.log(`Posicionando venda. Ganho de ${profitability}`);
+            console.log('Vendendo por: ' + price)
+            const sellOrder = await api.newOrder(symbol, 1,  price, 'SELL', 'LIMIT');
+            console.log(`orderId: ${sellOrder.orderId}`);
+            console.log(`status: ${sellOrder.status}`);
+
+        }
+    }
+    else if(buy>1000) console.log('Hora de vender!') // esse elseif é pra se eu quiser implementar
+    //alguma lógica de venda (atualmente estou colocando a venda em 10% do preço que comprei).
     else console.log('Esperando.')
-
+    
+   //console.log(await api.exchangeInfo())
 
 }, process.env.CRAWLER_INTERNAL);
 
